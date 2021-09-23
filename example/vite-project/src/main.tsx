0c49ce6d23cce5bc06bd0565b39b4d7ed4a0b79e
caosaiqi <626349071@qq.com>
2021-09-23 21:59:33 +0800

    layout 组件定于

---
+++ example/vite-project/src/main.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Layout } from '../../../src'
+
+ReactDOM.render(
+  <React.StrictMode>
+     <Layout />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
