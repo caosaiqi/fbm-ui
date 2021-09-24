eaacc54ec8287dd950ebada1b5b4e30b8337d669
caosaiqi <626349071@qq.com>
2021-09-24 16:51:45 +0800

    init

--- example/vite-project/src/main.tsx
+++ example/vite-project/src/main.tsx
@@ -2,10 +2,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Layout } from '../../../src'
+import {
+  FolderIcon, HomeIcon, LayerIcon, PeopleIcon, SettingIcon,
+} from '@avocadoui/icons';
 
-ReactDOM.render(
-  <React.StrictMode>
-     <Layout />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+const Main = () => {
+  const menuPrpos = {
+    menus: [
+      {
+        title: '首页',
+        icon: <HomeIcon />,
+        link:'/',
+        selected: true,
+      },
+      {
+        title: '人才库',
+        icon: <FolderIcon />,
+        link: '/',
+      },
+    ],
+  }
+  return (
+    <React.StrictMode>
+      <Layout menuPrpos={menuPrpos}>
+        <h1>hello world</h1>
+      </Layout>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
