776e8f3c51447262b8161212a49cade7782dfd8b
caosaiqi <626349071@qq.com>
2021-10-25 11:42:53 +0800

    feat: 修改package scripts

--- example/src/pages/dropdown/index.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import React from 'react'
-import { Dropdown } from "../../../../src"
-
-export default () => (<div>
-  <Dropdown>
-    asd
-  </Dropdown>
-</div>)
