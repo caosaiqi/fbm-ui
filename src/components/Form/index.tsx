83fdd2765ba30a5a2208cc1fbc932f269481c9d4
caosaiqi <626349071@qq.com>
2021-11-23 17:14:51 +0800

    update v

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Formik, useFormikContext} from 'formik';
+// import { Formik, useFormikContext} from 'formik';
 
-export const componentName: string = 'Form'
+// export const componentName: string = 'Form'
 
-interface FbmFormProps {
-  loading?: boolean;
-  formik: any
-}
+// interface FbmFormProps {
+//   loading?: boolean;
+//   formik: any
+// }
 
-const Form = ({
-  children
-}) => {
-  const props = useFormikContext()
-  console.log(props)
-  return <form action="">
+// const Form = ({
+//   children
+// }) => {
+//   const props = useFormikContext()
+//   return <form action="">
 
-    {children}
-  </form>
-}
+//     {children}
+//   </form>
+// }
 
-const FbmForm: React.FC<FbmFormProps> = (props) => {
-  const { children, formik } = props
-  return (
-    <Formik>
-      <Form>{children}</Form>
-    </Formik>
-  )
+const FbmForm: React.FC = (props) => {
+  // const { children, formik } = props
+  // return (
+  //   <Formik>
+  //     <Form>{children}</Form>
+  //   </Formik>
+  // )
+  return null
 }
 
 export default FbmForm
