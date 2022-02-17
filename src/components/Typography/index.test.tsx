5957db610b82190fd3d6d079db35d3bc0d4e3ff8
lizunlong <lizunlong@gmail.com>
2022-02-17 16:40:25 +0800

    test: <Select />

--- src/components/Typography/index.test.tsx
+++ src/components/Typography/index.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Typography from '.'
 
 describe('<Typography />', () => {
-  it('Typography render', () => {
+  it('should render', () => {
     const text = '张三'
     const { getByText, queryByText, unmount } = render(<Typography variant="body1">{text}</Typography>);
     expect(getByText(text)).toBeInTheDocument()
